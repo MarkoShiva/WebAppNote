3f6c0c04c45bc471bc90142c2baa8fd8f4a1572f
Marko Shiva Pavlovic <marko.shiva.pavlovic@gmail.com>
2020-02-21 00:51:30 +0100

    Initial commit

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+from sqlalchemy.dialects import registry
+
+
+class Config(object):
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dont-know-passphrase'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    ADMINS = ['admin@testapp.com']
+    SENDGRID_DEFAULT_FROM = 'admin@testapp.com'
+    MAIL_SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
+
+
