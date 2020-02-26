5a8e9c38720a902277439d8079013e7dfb39a7ad
Marko Shiva Pavlovic <marko.shiva.pavlovic@gmail.com>
2020-02-27 00:30:15 +0100

    cleanup

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,3 @@
-from datetime import datetime, timedelta
-
 from app import app, db
 from flask import render_template, flash, redirect, url_for, request, g, jsonify
 from app.forms import LoginForm, RegistrationForm, ResetPasswordForm, ResetPasswordRequestForm, NotesForm, TasksForm
@@ -7,7 +5,6 @@ from flask_login import current_user, login_user, logout_user
 from flask_login import current_user, login_required
 from app.models import User, Notes, Tasks
 from app.email import send_password_reset_email
-
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -18,11 +15,7 @@ def login():
         user = User.query.filter_by(email=form.email.data).first_or_404()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid email or password')
-            # if user exist in database the app will uniformly behave and return 404
             return render_template('errors/404.html', title="wrong credentials"), 404
-            # if the user exist app will return him to login page and not render 404 ypu can uncomment this line if that
-            # is preferred behaviour
-            # return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for("secret"))
     return render_template('auth/login.html', title='Sign In', form=form)
@@ -30,7 +23,7 @@ def login():
 
 @app.route('/')
 @app.route('/index')
-# @login_required
+@login_required
 def index():
     return render_template('index.html', title='Home')
 
